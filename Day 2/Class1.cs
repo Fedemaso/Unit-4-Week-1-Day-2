bf09ede80a4c776cdece86dc703b3087e354e5c4
fedemaso <fede.maso1989@gmail.com>
2023-08-29 16:18:14 +0200

    Aggiungere i file di progetto.

---
+++ Day 2/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2
+{
+    public class Persona
+    {
+        public Persona(string nome, string cognome, double età)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Età = età;
+        }
+
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+
+        public double Età { get; set; }
+
+        public string GetFullPersona()
+        {
+            return Nome + " " + Cognome + " " + Età;
+        }
+
+        public void GetFullPersonaProc() {
+            Console.WriteLine(Nome + " " + Cognome + " " + Età);
+        }
+
+
+    }
+
+
+
+    public class Prodotto
+    {
+
+        public Prodotto(string nomeProdotto, int prezzo, int quantità)
+        {
+            NomeProdotto = nomeProdotto;
+            Prezzo = prezzo;
+            Quantità = quantità;
+
+        }
+
+        public string NomeProdotto { get; set; }
+        public int Prezzo { get; set; }
+
+        public int Quantità { get; set; }
+
+
+        public int CostoTotale()
+        {
+            return (Prezzo * Quantità);
+        }
+
+
+        public void CostoTotaleProc()
+        {
+            Console.WriteLine(Prezzo * Quantità);
+
+        }
+
+
+
+    }
+}
---
+++ Day 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nome:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Cognome:");
+            string cognome = Console.ReadLine();
+
+            Console.WriteLine("Età:");
+            int età = Convert.ToInt32(Console.ReadLine());
+
+            Persona persona = new Persona(nome, cognome, età);
+            persona.GetFullPersonaProc();
+
+            Console.ReadLine();
+
+
+            Console.WriteLine("Nome Prodotto:");
+            string nomeProdotto = Console.ReadLine();
+
+            Console.WriteLine("Prezzo Prodotto:");
+            int prezzo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Quantità:");
+            int quantità = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Il costo totale è:");
+            Prodotto prodotto = new Prodotto(nomeProdotto, prezzo, quantità);
+            prodotto.CostoTotaleProc();
+
+           
+            Console.ReadLine();
+        }
+    }
+}
